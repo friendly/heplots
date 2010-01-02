4e80036430813877dd4bf95264b473afe43e9b5c
friendly <friendly@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-01-02 18:28:47 +0000

    Revised mark.H0 to handle 3D plots; added cross3d to NAMESPACE
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/heplots/pkg@48 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/mark.H0.R
+++ R/mark.H0.R
@@ -1,6 +1,18 @@
-mark.H0 <- function(x=0, y=0, cex=2, pch=19, col="green3", lty=2, pos=2) {
-	points(x,y, cex=cex, col=col, pch=pch)
-	text(x,y, expression(H[0]), col=col, pos=pos)
-	if (lty>0) abline(h=y, col=col, lty=lty)
-	if (lty>0) abline(v=x, col=col, lty=lty)
+# last modified 2 Jan 2010
+
+# mark the location of a point corresponding to a null hypothesis
+
+mark.H0 <- function(x=0, y=0, z=NULL, cex=2, pch=19, col="green3", lty=2, pos=2) {
+	if (is.null(z)) {
+		points(x,y, cex=cex, col=col, pch=pch)
+		text(x,y, expression(H[0]), col=col, pos=pos)
+		if (lty>0) abline(h=y, col=col, lty=lty)
+		if (lty>0) abline(v=x, col=col, lty=lty)
+	}
+	else {
+		bbox <- matrix(par3d("bbox"), nrow=2)
+		ranges <- apply(bbox, 2, diff)
+		points3d(x, y, z, size=5*cex, color=col)
+		if(lty>0) cross3d(c(x,y,z), (ranges/2), col=col, lty=lty)
+	}
 }
