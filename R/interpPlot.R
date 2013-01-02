6cfd85bfcab36229fe0fc14c25948ff44da0921e
friendly <friendly@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-02 21:25:51 +0000

    minor fix to interpPlot
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/heplots/pkg@151 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/interpPlot.R
+++ R/interpPlot.R
@@ -14,46 +14,31 @@ interpPlot <- function(xy1, xy2, alpha,
 		segments=FALSE, segment.col="darkgray",
 		 ...){
 
-	# interpolate
-	xy <- xy1 + alpha* (xy2-xy1)
-
-	# set default plot limits to incorporate xy1, xy2
-	lims <- apply(rbind(xy1, xy2), 2, range)
-	if (missing(xlim)) xlim=lims[,1]
-	if (missing(ylim)) ylim=lims[,2]
-
-	plot(xy, xlim=xlim, ylim=ylim, ...)
-  if (ellipse) {
-      ellipse.args <- c(list(xy, add = TRUE, plot.points = FALSE), 
-          ellipse.args)
-      do.call(dataEllipse, ellipse.args)
-  }
-  if (abline) {
-    abline(lsfit(xy[, 1], xy[, 2]), col = col.lines, 
-        lwd = lwd)
-  }
-  showLabels(xy[, 1], xy[, 2], labels = labels, id.method = id.method, 
-        id.n = id.n, id.cex = id.cex, id.col = id.col)
-  
-  if(segments) {
-  	segments(xy1[,1], xy1[,2], xy[,1], xy[,2], col=segment.col)
-  }
-   
+	 xy1 <- as.matrix(xy1)
+	 xy2 <- as.matrix(xy2)
+	 # interpolate
+	 xy <- xy1 + alpha * (xy2-xy1)
+	 
+	 # set default plot limits to incorporate xy1, xy2
+	 lims <- apply(rbind(xy1, xy2), 2, range)
+	 if (missing(xlim)) xlim=lims[,1]
+	 if (missing(ylim)) ylim=lims[,2]
+	 
+	 plot(xy, xlim=xlim, ylim=ylim, ...)
+	 if (ellipse) {
+		 ellipse.args <- c(list(xy, add = TRUE, plot.points = FALSE), 
+				 ellipse.args)
+		 do.call(dataEllipse, ellipse.args)
+	 }
+	 if (abline) {
+		 abline(lsfit(xy[, 1], xy[, 2]), col = col.lines, 
+				 lwd = lwd)
+	 }
+	 showLabels(xy[, 1], xy[, 2], labels = labels, id.method = id.method, 
+			 id.n = id.n, id.cex = id.cex, id.col = id.col)
+	 
+	 if(segments) {
+		 segments(xy1[,1], xy1[,2], xy[,1], xy[,2], col=segment.col)
+	 }
 }
 
-CIRCLETEST <-FALSE
-
-if(CIRCLETEST) {
-	# two circles
-	circle <- function(radius, segments=60) {
-			angles <- (0:segments)*2*pi/segments
-			radius * cbind( cos(angles), sin(angles))
-	}
-	
-	C1 <- circle(2)
-	C2 <- circle(4)
-	
-	for (a in seq(0,1,.1)) {
-		interpPlot(C1, C2, alpha=a, xlim=c(-4,4), ylim=c(-4,4), pch=16, col=rgb(1,0,0,a), type='b')
-	}
-}
