09a273c0904152721c8bf27e67ca6d611721ba5f
friendly <friendly@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-02-10 19:49:54 +0000

    added S3 methods for boxM
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/heplots/pkg@190 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/boxM.R
+++ R/boxM.R
@@ -1,22 +1,27 @@
+# Box's M-test for Homogeneity of Covariance Matrices
+
 boxM <-
-function(data, grouping)
+	function(Y, ...) UseMethod("boxM")
+
+
+boxM.default <- function(Y, group, ...)
 {
-   if (!inherits(data, c("data.frame", "matrix")))
-      stop("'data' must be a numeric data.frame or matrix!")
-   if (length(grouping) != nrow(data))
+   dname <- deparse(substitute(Y))
+   if (!inherits(Y, c("data.frame", "matrix")))
+      stop(paste(dname, "must be a numeric data.frame or matrix!"))
+   if (length(group) != nrow(Y))
       stop("incompatible dimensions!")
-   dname <- deparse(substitute(data))
-   data <- as.matrix(data)
-   grouping <- as.factor(as.character(grouping))
-   p <- ncol(data)
-   nlev <- nlevels(grouping)
-   lev <- levels(grouping)
-   dfs <- tapply(grouping, grouping, length) - 1
+   Y <- as.matrix(Y)
+   group <- as.factor(as.character(group))
+   p <- ncol(Y)
+   nlev <- nlevels(group)
+   lev <- levels(group)
+   dfs <- tapply(group, group, length) - 1
    if (any(dfs < p)) 
       warning("there are one or more levels with less observations than variables!")
    mats <- aux <- list()
    for(i in 1:nlev) {
-      mats[[i]] <- cov(data[grouping == lev[i], ])
+      mats[[i]] <- cov(Y[group == lev[i], ])
       aux[[i]] <- mats[[i]] * dfs[i]
    }
    names(mats) <- lev
@@ -30,12 +35,13 @@ function(data, grouping)
    dfchi <- (choose(p, 2) + p) * (nlev - 1)
    pval <- pchisq(X2, dfchi, lower.tail = FALSE)
 
-   means <- aggregate(data, list(grouping), mean)
+   means <- aggregate(Y, list(group), mean)
    rn <- as.character(means[,1])
    means <- means[,-1]
-   means <- rbind( means, colMeans(data) )
+   means <- rbind( means, colMeans(Y) )
    rownames(means) <- c(rn, "_all_")
 
+   logdet <- c(logdet, pooled=log(det(pooled)))
    out <- structure(
       list(statistic = c("Chi-Sq (approx.)" = X2),
          parameter = c(df = dfchi),
@@ -47,4 +53,34 @@ function(data, grouping)
       class = c("htest", "boxM")
       )
    return(out)
+}
+
+boxM.formula <- function(Y, data, ...)
+{
+	form <- Y
+	mf <- model.frame(form, data)
+	if (any(sapply(2:dim(mf)[2], function(j) is.numeric(mf[[j]])))) 
+		stop("Box's M test is not appropriate with quantitative explanatory variables.")
+
+	Y <- mf[,1]
+	if (dim(Y)[2] < 2) stop("There must be two or more response variables.")
+
+	if(dim(mf)[2]==2) group <- mf[,2]
+	else {
+		if (length(grep("\\+ | \\| | \\^ | \\:",form))>0) stop("Model must be completely crossed formula only.")
+		group <- interaction(mf[,2:dim(mf)[2]])
+	}
+	boxM.default(Y=Y, group=group, ...)
+
+}
+
+
+boxM.lm <- function(Y, ...) {
+  data <- getCall(Y)$data
+  Y <- if (!is.null(data)) {
+    data <- eval(data, envir = environment(formula(Y)))
+    update(Y, formula(Y), data = data)
+  }
+  else update(Y, formula(Y))
+	boxM.formula(formula(Y), data=eval(data, envir = environment(formula(Y))), ...)
 }
