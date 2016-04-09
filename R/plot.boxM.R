90e2b5f3cb4a8a2c5d7e9cb8107c7c509783a2e7
friendly <friendly@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-04-09 20:54:38 +0000

    plot.boxM now can plot other measures
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/heplots/pkg@223 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/plot.boxM.R
+++ R/plot.boxM.R
@@ -1,16 +1,31 @@
 plot.boxM <- 
   function(x, gplabel = NULL,
+           which = c("logDet", "product", "sum", "precision", "max"),
+           log = which=="product",
            pch=c(16, 15), 
            cex=c(2, 2.5), 
            col=c("blue", "red"),
            xlim,
-           conf=0.95, method=2, bias.adj=TRUE, lwd=2, ...) {
+           conf=0.95, method=1, bias.adj=TRUE, lwd=2, ...) {
     
-    dets <- x$logDet
-    ng <- length(dets)-1
+    which <- match.arg(which)
+    if (which=="logDet") {
+      measure <- x$logDet
+      xlab <- "log determinant"
+    }
+    else {
+      eigstats <- summary(x, quiet=TRUE)$eigstats
+      measure <- eigstats[which,]
+      if(log) measure <- log(measure)
+      xlab <- paste(if(log) "log" else "", which, "of eigenvalues" )
+      conf <- 0
+    }
+    
+    
+    ng <- length(measure)-1
     
     if (missing(xlim)) {
-      xlim <- range(dets)
+      xlim <- range(measure)
     }
     
     if (conf>0) {
@@ -21,11 +36,11 @@ plot.boxM <-
       xlim[2] <- max(xlim[2], CI$upper)
     }
     
-    dotchart(dets, xlab = "log determinant", xlim=xlim,  ...)
+    dotchart(measure, xlab = xlab, xlim=xlim,  ...)
     if (conf>0) {
       arrows(CI$lower, 1:(ng+1), CI$upper, 1:(ng+1), lwd=lwd, angle=90, length=.075, code=3)
     }
-    points(dets, 1:(ng+1),  
+    points(measure, 1:(ng+1),  
            cex=c(rep(cex[1], ng), cex[2]), 
            pch=c(rep(pch[1], ng), pch[2]),
            col=c(rep(col[1], ng), col[2]))
@@ -34,3 +49,24 @@ plot.boxM <-
       text(par("usr")[1], ng + .5, gplabel, pos=4, cex=1.25)
     
   }
+
+
+# TESTME <- FALSE
+# if(TESTME) {
+# 
+# data(Skulls, package="heplots")
+# # make shorter labels for epochs
+# Skulls$epoch <- ordered(Skulls$epoch, labels=sub("c","",levels(Skulls$epoch)))
+# skulls.mod <- lm(cbind(mb, bh, bl, nh) ~ epoch, data=Skulls)
+# skulls.boxm <- boxM(skulls.mod)
+# 
+# plot(skulls.boxm, gplabel="Epoch")
+# 
+# op <- par(mfrow=c(2,2), mar=c(5,4,1,1))
+# plot_eigstats(skulls.boxm, which="product", gplabel="Epoch", xlim=c(10,14))
+# plot_eigstats(skulls.boxm, which="sum", gplabel="Epoch")
+# plot_eigstats(skulls.boxm, which="precision", gplabel="Epoch")
+# plot_eigstats(skulls.boxm, which="max", gplabel="Epoch")
+# par(op)
+# 
+# }
