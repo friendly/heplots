69bba019a303ed583c23be7b2ecba6a10a9a891d
friendly <friendly@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-04-17 21:46:07 +0000

    added bartlettTests() for a collection of univariate Bartlett tests
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/heplots/pkg@226 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/bartlettTests.R
@@ -0,0 +1,38 @@
+# univariate Bartlett tests for a multivariate response
+
+bartlettTests <-
+	function (y, group, ...) 
+{
+	if (! inherits(y, "data.frame") | inherits(y, "matrix") )
+		stop(deparse(substitute(y)), " is not a data frame or matrix")
+	if (! all( apply(y, 2, is.numeric) ))
+		stop(deparse(substitute(y)), " has non-numeric columns")
+
+	btest <- apply(y, 2, FUN=function(x) stats::bartlett.test(x, g=group, ...))
+	BT <- as.data.frame(matrix(0, nrow=length(btest), 3))
+
+	for (i in 1:nrow(BT)) {
+		BT[i,1] <- btest[[i]]$statistic
+		BT[i,2] <- btest[[i]]$parameter
+		BT[i,3] <- btest[[i]]$p.value
+	}
+	rownames(BT) <- names(btest)
+	colnames(BT) <- c("Chisq", "df", "Pr(>Chisq)")
+	class(BT) <- c("anova", "data.frame")
+  dots <- deparse(substitute(...))
+	attr(BT, "heading") <- paste("Bartlett's Tests for Homogeneity of Variance",
+	       if (!(dots == "NULL"))  paste(":", dots), "\n")
+	BT
+}
+
+TESTME <- FALSE
+if (TESTME) {
+bartlettTests(iris[,1:4], iris$Species)
+
+data(Skulls, package="heplots")
+bartlettTests(Skulls[,-1], Skulls$epoch)
+
+data(Wine, package="candisc")
+bartlettTests(Wine[,-1], Wine$Cultivar)
+}
+
