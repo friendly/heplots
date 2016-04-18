b7a77636cbe8b6f15fed3b5e2ba047db421a3cc2
friendly <friendly@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-04-18 14:27:26 +0000

    added leveneTests()
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/heplots/pkg@228 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/leveneTests.R
@@ -0,0 +1,42 @@
+# univariate Levene tests for a multivariate response
+
+leveneTests <-
+	function (y, group, center = median, ...) 
+{
+	if (! inherits(y, "data.frame") | inherits(y, "matrix") )
+		stop(deparse(substitute(y)), " is not a data frame or matrix")
+	if (! all( apply(y, 2, is.numeric) ))
+		stop(deparse(substitute(y)), " has non-numeric columns")
+
+	ltest <- apply(y, 2, FUN=function(x) car::leveneTest(x, group=group, center=center, ...))
+	LT <- as.data.frame(matrix(0, nrow=length(ltest), 4))
+	
+	for (i in 1:nrow(LT)) {
+		LT[i,1] <- ltest[[i]][1,1]
+		LT[i,2] <- ltest[[i]][2,1]
+		LT[i,3] <- ltest[[i]][1,2]
+		LT[i,4] <- ltest[[i]][1,3]	
+	}
+	colnames(LT) <- c("df1", "df2", "F value", "Pr(>F)")
+	rownames(LT) <- names(ltest)
+	class(LT) <- c("anova", "data.frame")
+  dots <- deparse(substitute(...))
+  attr(LT, "heading") <- paste("Levene's Tests for Homogeneity of Variance (center = ", 
+      deparse(substitute(center)), if (!(dots == "NULL")) 
+          paste(":", dots), ")\n", sep = "")
+	LT
+}
+
+TESTME <- FALSE
+if (TESTME) {
+leveneTests(iris[,1:4], iris$Species)
+
+# handle a 1-column response?
+leveneTests(iris[,1, drop=FALSE], iris$Species)
+
+data(Skulls, package="heplots")
+leveneTests(Skulls[,-1], Skulls$epoch)
+
+data(Wine, package="candisc")
+leveneTests(Wine[,-1], Wine$Cultivar)
+}
