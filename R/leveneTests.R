28a2d9723d889ffc717536959f4f6509fe01c729
friendly <friendly@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-05-05 15:20:14 +0000

    convert some .R to roxygen
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/heplots/pkg@238 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/leveneTests.R
+++ R/leveneTests.R
@@ -1,5 +1,47 @@
 # univariate Levene tests for a multivariate response
 
+
+
+#' Levene Tests of Homogeneity of Variances
+#' 
+#' This function extends \code{\link[car]{leveneTest}} to a multivariate
+#' response setting.  It performs the Levene test of homogeneity of variances
+#' for each of a set of response variables, and prints a compact summary.
+#' 
+#' 
+#' @param y A data frame or matrix of numeric response variables in a
+#' multivariate linear model.
+#' @param group a vector or factor object giving the group for the
+#' corresponding elements of the rows of \code{y}
+#' @param center The name of a function to compute the center of each group;
+#' \code{mean} gives the original Levene's (1960) test; the default,
+#' \code{median}, provides a more robust test suggested by Brown and Forsythe
+#' (1974).
+#' @param \dots other arguments, passed to \code{\link[car]{leveneTest}}
+#' @return An object of classes "anova" and "data.frame", with one observation
+#' for each response variable in \code{y}.
+#' @author Michael Friendly
+#' @seealso \code{\link[car]{leveneTest}}, \code{\link{bartlettTests}}
+#' @references Levene, H. (1960). Robust Tests for Equality of Variances. In
+#' Olkin, I. \emph{etal.} (Eds.), \emph{Contributions to Probability and
+#' Statistics: Essays in Honor of Harold Hotelling}, Stanford University Press,
+#' 278-292.
+#' 
+#' Brown, M. B. & Forsythe, A. B. (1974). Robust Tests For Equality Of
+#' Variances \emph{Journal of the American Statistical Association}, \bold{69},
+#' 364-367.
+#' @keywords htest
+#' @examples
+#' 
+#' leveneTests(iris[,1:4], iris$Species)
+#' 
+#' # handle a 1-column response?
+#' leveneTests(iris[,1, drop=FALSE], iris$Species)
+#' 
+#' data(Skulls, package="heplots")
+#' leveneTests(Skulls[,-1], Skulls$epoch)
+#' 
+#' 
 leveneTests <-
 	function (y, group, center = median, ...) 
 {
--- R/trans.colors.R
+++ R/trans.colors.R
@@ -2,6 +2,45 @@
 # alpha transparency: (0 means fully transparent and 1 means opaque).
 # names: optional character vector of names for the colors
 
+
+
+#' Make Colors Transparent
+#' 
+#' Takes a vector of colors (as color names or rgb hex values) and adds a
+#' specified alpha transparency to each.
+#' 
+#' Colors (\code{col}) and \code{alpha} need not be of the same length. The
+#' shorter one is replicated to make them of the same length.
+#' 
+#' @param col A character vector of colors, either as color names or rgb hex
+#' values
+#' @param alpha alpha transparency value(s) to apply to each color (0 means
+#' fully transparent and 1 means opaque)
+#' @param names optional character vector of names for the colors
+#' @return A vector of color values of the form \code{"#rrggbbaa"}
+#' @author Michael Friendly
+#' @seealso %% ~~objects to See Also as \code{\link{help}}, ~~~
+#' \code{\link[grDevices]{col2rgb}}, \code{\link[grDevices]{rgb}},
+#' \code{\link[grDevices]{adjustcolor}},
+#' @keywords color
+#' @examples
+#' 
+#' trans.colors(palette(), alpha=0.5)
+#' 
+#' # alpha can be vectorized
+#' trans.colors(palette(), alpha=seq(0, 1, length=length(palette())))
+#' 
+#' # lengths need not match: shorter one is repeated as necessary
+#' trans.colors(palette(), alpha=c(.1, .2))
+#' 
+#' trans.colors(colors()[1:20])
+#' 
+#' # single color, with various alphas
+#' trans.colors("red", alpha=seq(0,1, length=5))
+#' # assign names
+#' trans.colors("red", alpha=seq(0,1, length=5), names=paste("red", 1:5, sep=""))
+#' 
+#' 
 trans.colors <- function(col, alpha=0.5, names=NULL) {
   nc <- length(col)
   na <- length(alpha)
