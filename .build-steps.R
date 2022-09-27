1ea8e6f802f904d474b8bc160755d924951b92fc
Michael Friendly <friendly@yorku.ca>
2022-09-27 17:50:48 -0400

    add pkg versions to vignettes

--- .build-steps.R
+++ .build-steps.R
@@ -8,30 +8,39 @@ Sys.setenv(RGL_USE_NULL = TRUE)
 # it was necessary to build manually, using
 
 #Sys.setenv(R_GSCMD="C:/Program Files/gs/gs9.21/bin/gswin64c.exe")
-Sys.setenv(R_GSCMD="C:/Program Files/gs/gs9.53.3/bin/gswin64c.exe")
+#Sys.setenv(R_GSCMD="C:/Program Files/gs/gs9.53.3/bin/gswin64c.exe")
 
 # revdep
 #remotes::install_github("r-lib/revdepcheck")
 library(revdepcheck)
 revdep_check(num_workers = 4)
 
-
-devtools::build(args = c('--resave-data','--compact-vignettes=both'))
-
-#devtools::build(args = c('--compact-vignettes=both'))
-
-# then, test with win builder
-args = c('--resave-data','--compact-vignettes=both')
-devtools::check_win_devel(args=args)
-
-# submit to cran
-args = c('--resave-data','--compact-vignettes=both')
-#devtools::submit_cran(args=args)
-devtools::release(args=args)
-
-
-# NB: building vignettes, then compacting
+devtools::build()
 devtools::build_vignettes()
 
-tools::compactPDF("doc/HE-examples.pdf", gs_quality="ebook")
-# compacted ‘HE-examples.pdf’ from 765Kb to 415Kb
+devtools::check_win_devel()
+
+# submit to cran
+devtools::release()
+
+
+# no longer need this
+# devtools::build(args = c('--resave-data','--compact-vignettes=both'))
+# 
+# #devtools::build(args = c('--compact-vignettes=both'))
+# 
+# # then, test with win builder
+# args = c('--resave-data','--compact-vignettes=both')
+# devtools::check_win_devel(args=args)
+
+# # submit to cran
+# args = c('--resave-data','--compact-vignettes=both')
+# #devtools::submit_cran(args=args)
+# devtools::release(args=args)
+
+# 
+# # NB: building vignettes, then compacting
+# devtools::build_vignettes()
+# 
+# tools::compactPDF("doc/HE-examples.pdf", gs_quality="ebook")
+# # compacted ‘HE-examples.pdf’ from 765Kb to 415Kb
