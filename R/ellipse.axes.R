917b369f62369a1ee2a1d1e73d414b4c0a4dd139
Michael Friendly <friendly@yorku.ca>
2023-12-05 09:49:22 -0500

    fix min/max bug in ellipse.axes

--- R/ellipse.axes.R
+++ R/ellipse.axes.R
@@ -1,6 +1,6 @@
 #' Draw Axes of a 2D Covariance Ellipse
 #' 
-#' A function to draw the major axes of a 2D ellipse from a correlation,
+#' A function to draw the principal axes of a 2D ellipse from a correlation,
 #' covariance or sums of squares and cross products matrix in an existing plot.
 #' 
 #' 
@@ -12,26 +12,26 @@
 #' @param scale If x is a correlation matrix, then the standard deviations of
 #'          each parameter can be given in the scale parameter.  This defaults to
 #'          \code{c(1, 1)}, so no rescaling will be done.
-#' @param level The coverage level of a simultaneous region.  The
+#' @param level The coverage level of a simultaneous region of the ellipse.  The
 #'          default is 0.95, for a 95\% region.  This is used to control the size of the
-#'          ellipsoid.
-#' @param t The size of the ellipsoid may also be controlled by specifying the
+#'          ellipse.
+#' @param radius The size of the ellipsoid may also be controlled by specifying the
 #'          value of a t-statistic on its boundary, which defaults to the square root of a chi-square statistic
 #'          for a given \code{level} on 2 degrees of freedom.
 #' @param which An integer vector to select which variables from the object \code{x} will be
 #'          plotted.  The default is the first 2.
 #' @param labels Either a logical value, a character string, or a character
-#'          vector of length 2.  If \code{TRUE}, the default, the axes are labeled PC1,
-#'          PC2.  If a single character string, the digits 1, and 2 are pasted on
+#'          vector of length 2.  If \code{TRUE}, the default, the axes are labeled "PC1",
+#'          "PC2".  If a single character string, the digits 1, and 2 are pasted on
 #'          the end.
 #' @param label.ends A vector of indices in the range \code{1:4} indicating which ends of the axes
 #'          should be labeled, corresponding to a selection of rows of the 4 x 2 matrix
-#'          of axes end points. Values \code{1:2} represent the minimum and maximum of the first dimension respectivly.
+#'          of axes end points. Values \code{1:2} represent the minimum and maximum of the first dimension respectively.
 #'          Values \code{3:4} represent the minimum and maximum of the second dimension.
 #'          Default: \code{c(2, 4)}.
 #' @param label.pos Positions of text labels relative to the ends of the axes used in \code{\link[graphics]{text}} for
 #'          the four possible \code{label.ends}. 1, 2, 3, 4 represent below, to the left, above and to the right.
-#'          Default: \code{c(4, 2, 3, 1)}.
+#'          The default, \code{c(2, 4, 1, 3)}, positions the labels outside the axes.
 #' @param \dots Other arguments passed to \code{lines} and \code{text}.
 #'
 #' @return Invisibly returns a 4 x 2 matrix containing the end points of the axes in pairs (min, max) by rows.
@@ -47,7 +47,6 @@
 #' radius <- sqrt(qchisq(0.68, 2))
 #' plot(iris[,1:2])
 #' car::ellipse(mu, cov, radius = radius)
-#' # there's a bug here: labels appear at min of PC1 & PC2
 #' res <- ellipse.axes(cov, centre=mu, level = 0.68,
 #'             labels = TRUE)
 #' res
@@ -59,11 +58,11 @@ ellipse.axes <- function(
     center = center,
     scale = c(1, 1), 
     level = 0.95,
-    t = sqrt(qchisq(level, 2)), 
+    radius = sqrt(qchisq(level, 2)), 
     which = 1:2, 
     labels = TRUE, 
     label.ends = c(2, 4),
-    label.pos = c(4, 2, 3, 1),
+    label.pos = c(2, 4, 1, 3),
     ...) 
 {
   stopifnot(is.matrix(x)) 
@@ -79,17 +78,21 @@ ellipse.axes <- function(
        1, 0,
        0, -1,
        0, 1), nrow = 4, ncol = 2, byrow = TRUE)
-  rownames(axes)<- apply(expand.grid(c("X","Y"), c("min","max")),
+  rownames(axes)<- apply(expand.grid(c("min","max"), c("X","Y"))[,2:1],
                          1, paste, collapse="")
 
   # transform to PC axes
   result <- axes %*% sqrt(diag(eig$values)) %*% t(eig$vectors)
   # scale
   result <- result %*% diag(rep(t, 2))
-  # center at values of centre
+  # center 
   result <- sweep(result, 2L, centre, FUN="+")
   colnames(result) <- colnames(cov)
-
+  
+  # fix min/max
+  if(result[1,1] > result[2,1]) result[1:2,] <- result[2:1,]
+  if(result[3,2] > result[4,2]) result[3:4,] <- result[4:3,]
+  
   # convert to something that can be used by lines()
   #result <- rbind(result[1:2,], NA, result[3:4,])
 
@@ -110,9 +113,9 @@ ellipse.axes <- function(
 
 if(FALSE){
 # data(iris)
-# cov <- cov(iris[,1:2])
-# mu <- colMeans(iris[,1:2])
-# 
+cov <- cov(iris[,1:2])
+mu <- colMeans(iris[,1:2])
+
 radius <- sqrt(qchisq(0.68, 2))
 plot(iris[,1:2])
 car::ellipse(mu, cov, radius = radius)
@@ -121,7 +124,7 @@ res <- ellipse.axes(cov, centre=mu, level = 0.68,
                     lwd = 2, col = "red", cex = 1.2)
 
 # try using arrows
-arrows(mu[1], mu[2], x1 = res[,1], y1 = res[,2], angle = 15)
+arrows(mu[1], mu[2], x1 = res[,1], y1 = res[,2], angle = 15, length = .15)
 
 plot(iris[,1:2])
 car::ellipse(mu, cov, radius = radius)
