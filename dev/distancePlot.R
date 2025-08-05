e1971b9a36c3c4a6c2be261d04c48a00031a4047
Michael Friendly <friendly@yorku.ca>
2025-08-05 13:30:19 -0400

    edits/docs for dev/distancePlot.R

--- dev/distancePlot.R
+++ dev/distancePlot.R
@@ -2,24 +2,36 @@
 
 #' Distance Plot for Multivariate Model Diagnosis
 #' 
-#' @description The plot, suggested by Rousseeu et al. () typically plots Mahalanobis distances of the \code{Y} response
-#' variables against the distances of the \code{X} variables in a multivariate linear model.
-#' When applied to a multivariate linear model itself, it plots the distances of the residuals for the \code{Y} variables
-#' against the predictor terms in the model.matrix \code{X} 
+#' @description This plot, suggested by Rousseeu et al. (2004) typically plots Mahalanobis distances (\eqn{D}) of the \code{Y} response
+#' variables against the distances of the \code{X} variables in a multivariate linear model (MLM).
+#' When applied to a multivariate linear model itself, it plots the distances of the \emph{residuals} for the \code{Y} variables
+#' against the predictor terms in the \emph{model.matrix} \code{X}.
+#' 
+#' This diagnostic plot combines the information on regression outliers and leverage points, and is much more useful than either distance separately.
+#' 
+#' @details
+#' 
+#' Observations with "large" distances on \code{X} or \code{Y} are labeled with their \code{ids}. The cutoffs are calculated as 
+#' \eqn{\sqrt{\chi^2_{k, \text{level}}}}.
 #'
-#' @param X 
-#' @param Y 
-#' @param method 
-#' @param level 
-#' @param ids 
-#' @param pch 
-#' @param col 
-#' @param label.pos 
-#' @param xlab 
-#' @param ylab 
+#' @param X       A numeric data frame giving the predictors in the MLM
+#' @param Y       A numeric data frame giving the responses in the MLM or the residuals
+#' @param method  Estimation method used for center and covariance, one of: \code{"classical"} (product-moment), 
+#'                \code{"mcd"} (minimum covariance determinant), or \code{"mve"} (minimum volume ellipsoid). 
+#' @param level   Lower-tail probability beyond which observations will be labeled.
+#' @param ids     Labels for observations
+#' @param pch     Point symbol
+#' @param col     Color
+#' @param label.pos  Position of the label relative to the point 
+#' @param xlab    Label stub for horizontal axis
+#' @param ylab    Label stub for vertical axis
 #' @param ... 
 #'
-#' @return
+#' @return        Returns invisibly a data frame containing the 
+#' @seealso \code{\link{Mahalanobis}}
+#' @references 
+#' Rousseeuw, P. J., Van Driessen, K., Van Aelst, S., & Agullo, J. (2004). Robust multivariate regression. 
+#' \emph{Technometrics}, \bold{46}(3), 293–305. \doi{10.1198/004017004000000329}.
 #' @export
 #'
 #' @examples
@@ -30,11 +42,17 @@ distancePlot.default <- function(X, Y,
                          pch = c(1, 16),
                          col = c("black", "red"),
                          label.pos = 2,
-                         xlab = "Mahalanobis distances of X",
-                         ylab = "Mahalanobis distances of Y",
+                         xlab,
+                         ylab,
                          ...) {
   
-  if (nrow(X) != nrow(Y)) stop("Number of rows in X and Y differ.")
+  if (nrow(X) != nrow(Y)) stop(paste("Number of rows in", deparse(substitute(X)), "and", deparse(substitute(X)), "differ."))
+  
+  numeric_X <- sapply(X, is.numeric)
+  if (!all(numeric_X)) stop("All columns in X must be numeric")
+  numeric_Y <- sapply(Y, is.numeric)
+  if (!all(numeric_Y)) stop("All columns in Y must be numeric")
+  
   method = match.arg(method)
   method.case <- ifelse(method == "classical", "Classical", toupper(method))
   xlab <- paste(method.case, xlab)
@@ -50,7 +68,11 @@ distancePlot.default <- function(X, Y,
   out <- (distX > cutoffs[1]) | distY > cutoffs[2]
   out.rows <- which(out)
   
-#  browser()
+  # browser()
+  
+  if(missing(xlab)) xlab <- paste("Mahalanobis distances of", deparse(substitute(X)))
+  if(missing(ylab)) ylab <- paste("Mahalanobis distances of", deparse(substitute(Y)))
+  
   plot(distX, distY,
        pch = ifelse(out, pch[2], pch[1]),
        col = ifelse(out, col[2], col[1]),
@@ -85,8 +107,9 @@ distancePlot.formula <- function(X, Y, ...) {
 distancePlot.mlm <- function(X, ...) {
   Y <- residuals(X)
   X <- model.matrix(X)[,-1]
+  ylab <- "Mahalanobis distances of residuals"
 
-  distancePlot.default(X, Y, ...)
+  distancePlot.default(X, Y, ylab = ylab, ...)
 } 
 
 
