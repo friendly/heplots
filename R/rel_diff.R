faa6d6ccc325f283f350545805d609cfc1862af6
Michael Friendly <friendly@yorku.ca>
2025-08-08 12:47:20 -0400

    fix export rel_diff

--- R/rel_diff.R
+++ R/rel_diff.R
@@ -6,6 +6,11 @@
 #' Calculates the relative difference, defined as
 #' \deqn{\frac{\vert x - y \vert}{x} }
 #' between two arrays or data frames, where \code{x} are considered reference values.
+#' 
+#' @details
+#' Beyond the obvious, a natural use case is to compare coefficients for alternative models for the same data, e.g., a classical
+#' and a robust model.
+#' 
 #'
 #' @param x  An array or data frame, considered the reference values
 #' @param y  Comparison array or data frame
@@ -13,12 +18,22 @@
 #' @param epsilon Threshold for values near zero
 #' @return An array or data frame the same size as \code{x} and \code{y} containing the relative differences
 #' @export
+#' @seealso \code{link{robmlm}}
 #' @examples
-#' # example code
+#' # simple example
 #' m1 <- cbind(c(0,1), c(1,1))
 #' m2 <- cbind(c(0,1), c(1.01,1.11))
 #' rel_diff(m1, m2, pct = FALSE) 
 #' rel_diff(m1, m2) 
+#' 
+#' # compare coefficients
+#' data(Skulls)
+#' 
+#' # fit manova model, classically and robustly
+#' sk.mlm <- lm(cbind(mb, bh, bl, nh) ~ epoch, data=Skulls)
+#' sk.rlm <- robmlm(cbind(mb, bh, bl, nh) ~ epoch, data=Skulls)
+#' rel_diff(coef(sk.mlm),
+#'          coef(sk.rlm))
 
 rel_diff <- function(x, y,
                      pct = TRUE,
